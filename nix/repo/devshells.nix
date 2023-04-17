2a300be70cfed1700c9f09bdcc4899a72bc3decc
Lord-Valen <lord_valen@protonmail.com>
2023-04-17 17:00:48 -0400

    chore: add devshell commands

--- nix/repo/devshells.nix
+++ nix/repo/devshells.nix
@@ -9,7 +9,19 @@ in
   lib.mapAttrs (_: std.lib.dev.mkShell) {
     default = {
       name = "chatwith";
-			commands = [{package = inputs.nixpkgs.prefetch-npm-deps;}];
+			commands = [
+        {package = inputs.nixpkgs.prefetch-npm-deps;}
+        {
+          name = "upd";
+          help = "Update packages and prefetch deps.";
+          command = ''npm update && prefetch-npm-deps "$PRJ_ROOT"/package-lock.json'';
+        }
+        {
+          name = "dev";
+          help = "Run the program and watch for changes. For lazy people.";
+          command = ''npm run dev'';
+        }
+      ];
       nixago = with cell.configs; [
         editorconfig
         presets.conform
