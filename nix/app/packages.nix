90d3464e1abd31d868abdc8e5bde97c6bd4b869b
Lord-Valen <lord_valen@protonmail.com>
2023-04-18 21:31:28 -0400

    chore: upgrade to meow 11

--- nix/app/packages.nix
+++ nix/app/packages.nix
@@ -2,21 +2,21 @@
   inputs,
   cell,
 }: let
-	inherit (inputs) nixpkgs std self;
+  inherit (inputs) nixpkgs std self;
 in rec {
   default = chatwith;
   chatwith = nixpkgs.buildNpmPackage {
-		pname = "chatwith";
-		version = "0.1.2";
+    pname = "chatwith";
+    version = "0.1.2";
 
-		src = std.incl self [
-			"package.json"
-			"package-lock.json"
-			"tsconfig.json"
-			"global.d.ts"
-			"src"
-		];
+    src = std.incl self [
+      "package.json"
+      "package-lock.json"
+      "tsconfig.json"
+      "global.d.ts"
+      "src"
+    ];
 
-		npmDepsHash = "sha256-y9TuuSYUTfmYJ92Wo47ELlzNBHRHNEZrQFL4vVrlkrU=";
-	};
+    npmDepsHash = "sha256-TumJWW5uHZIm2q7ijHHINZ4e26Sz0qnkYJCGwDTcvU0=";
+  };
 }
