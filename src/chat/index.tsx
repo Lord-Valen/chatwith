dbe07ea29b6f0338b6f1267aad9050ec025aa6f4
Lord-Valen <lord_valen@protonmail.com>
2023-04-18 23:51:51 -0400

    feat: add help flag

--- src/chat/index.tsx
+++ src/chat/index.tsx
@@ -7,6 +7,22 @@ export * from './types.js';
 import React from 'react';
 
 import { render } from 'ink';
+import meow from "meow";
 import { App } from './App/index.js';
+
+const cli = meow(`
+  Usage
+    $ chatwith chat [Options]
+
+  Options
+    --help, -h     show help
+`, {
+    importMeta: import.meta,
+    flags: {
+        help: { type: "boolean", alias: "h" },
+    },
+});
+
+if (cli.flags.help) cli.showHelp()
 
 render(<App />);
